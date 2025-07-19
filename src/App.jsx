85a6a16e37e51216274f19d26675eb18013faf7b
Malia Wakesho-Ajwang <maz.obiri@emory.edu>
2025-07-19 11:54:44 +0800

    added newletter to the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Features from './components/Features';
 import About from './components/About';
 import Contact from './components/Contact';
+import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Features />
         <About />
         <Contact />
+        <Newsletter />
       </main>
       <Footer />
     </>
