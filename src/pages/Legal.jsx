596569fdf145ed94957bebb1cd45f5c17543c625
Malia Wakesho-Ajwang <maz.obiri@emory.edu>
2025-09-13 22:28:42 +0200

    the last commit Closes #8, #12, #13 and #14. I didn't create the pages that the footer links to but i did make the links

---
+++ src/pages/Legal.jsx
@@ -0,0 +1,10 @@
+export default function Legal() {
+    return (
+        <main className="main">
+            <p>legal ... this page is coming soon</p>
+            {/* <p><Link to="/privacy-policy">Privacy Policy</Link></p>
+            <p><Link to="/terms-of-service">Terms of Service</Link></p>
+            <p><Link to="/cookie-policy">Cookie Policy</Link></p> */}
+        </main>
+    );
+}
